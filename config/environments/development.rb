d266fe8da3c559a95ee749008c6e88816930c6b8
Ant <alonezuko@mail.ru>
2015-03-28 18:16:38 +0600

    add simple email sending

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,18 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.action_mailer.delivery_method = :smtp
+
+   config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "frey.mac.book",
+      authentication: "plain",
+      user_name: "whoisfrey",
+      password: "P0stfr3ik0",
+      enable_starttls_auto: true
+   }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
